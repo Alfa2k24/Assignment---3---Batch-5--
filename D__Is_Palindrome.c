9ae78cf294e021073c4337c2e7dd679c76fa02bd
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-05-03 02:08:26 +0600

    Solution Accepted

--- D__Is_Palindrome.c
+++ D__Is_Palindrome.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
-#include<string.h>>
-int main(){
+#include<string.h>
 
+// Is Palindrome Function 
+int is_palindrome(char inputString[]){
+    int stringLength = strlen(inputString);
+    for(int i = 0; i < stringLength/2; i++){
+        if(inputString[i] != inputString[stringLength - 1 - i]){
+            return 0;
+        }
+    }
+    return 1;
 }
+
+// Main Function
+int main(){
+char str[1000];
+scanf("%s",str);
+int isPalindrome =  is_palindrome(str);
+if(isPalindrome == 1){
+    printf("Palindrome\n");
+} else{
+    printf("Not Palindrome\n");
+}
+ return 0;
+ }
