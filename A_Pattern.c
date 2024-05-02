be1d1decf411c01c617d9da063542dfe3353c30a
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-05-02 23:45:48 +0600

    Solution Accepted

---
+++ A_Pattern.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int space = n - 1;
+    int star = 1;
+
+    for(int i = 1; i <= (2*n - 1); i++){
+        for(int j = 1; j <= space; j++){
+            printf(" ");
+        }
+        for(int j = 1; j <= star; j++){
+            if(i % 2 == 0){
+                printf("-");
+            }
+            else{
+                printf("#");
+            }
+        }
+        if( i < n){
+            space--;
+            star += 2;
+        } else{
+            space++;
+            star -= 2;
+        }
+        printf("\n");
+    }
+
+}
