38ea937fedc8f8666e1e27b280a49506c6b5ca38
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-05-03 01:05:30 +0600

    assignment accepted

--- B__Pattern_2.c
+++ B__Pattern_2.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 int main(){
 
-    // code
+    int n; 
+    scanf("%d",&n);
+    int space = n -1, num = 1;
+    for(int i = 1; i <= n;  i++){
+        for(int j = 1; j <= space;j++){
+            printf(" ");
+        }
+        for(int j = num; j >= 1; j--){
+            printf("%d",j);
+        }
+        space--;
+        num++;
+        printf("\n");
+    }
 
 }
+
+
+//Accepted
